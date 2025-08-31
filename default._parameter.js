d6190b413b55f4b442e741604b3ad9467aeaf5e0
Mohsin Abdullah Chowdhury <mohsinabd71@gmail.com>
2025-08-31 15:14:10 +0600

    ongoing

---
+++ default._parameter.js
@@ -0,0 +1,6 @@
+function sum(num1, num2){
+    const total = num1 + num2;
+    console.log(total)
+}
+
+sum(5, 10);
---
+++ let-const.js
@@ -0,0 +1,26 @@
+//es6
+
+const name = 'Bangladesh';
+
+const countryName = 'Bd' + name;
+
+console.log(countryName);
+
+
+//let give chance to change variable
+
+let price = 150;
+
+price = 160;
+
+console.log(price);
+
+
+const dress = ['shirt', 'pant', 'jacket'];
+dress.push('maflar')
+console.log(dress)
+
+const student = {name: 'jaga', marks: 70}
+student.marks = 80;
+
+console.log(student);
